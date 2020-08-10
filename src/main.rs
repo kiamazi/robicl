d9a6d68c80533aa111497dd003c05bb2e1e42515
kiavash <kiavash@gmail.com>
2020-08-10 13:18:35 +0430

    coed cleaning

--- src/main.rs
+++ src/main.rs
@@ -9,46 +9,46 @@ use geo_types::Point;
 
 fn main() {
     let now_time: DateTime<Local> = Local::now();
-    let hnow = now_time.format("%H").to_string();
-    let mnow = now_time.format("%M").to_string();
-    let snow = now_time.format("%S").to_string();
+    let hour_now   = now_time.format("%H").to_string();
+    let minute_now = now_time.format("%M").to_string();
+    let second_now = now_time.format("%S").to_string();
 
-    let c1 = format!( "{0:04b}", hnow[0..1].parse::<i8>().unwrap() );
-    let c2 = format!( "{0:04b}", hnow[1..2].parse::<i8>().unwrap() );
-    let c3 = format!( "{0:04b}", mnow[0..1].parse::<i8>().unwrap() );
-    let c4 = format!( "{0:04b}", mnow[1..2].parse::<i8>().unwrap() );
-    let c5 = format!( "{0:04b}", snow[0..1].parse::<i8>().unwrap() );
-    let c6 = format!( "{0:04b}", snow[1..2].parse::<i8>().unwrap() );
+    let column_one   = format!( "{0:04b}", hour_now[0..1].parse::<i8>().unwrap() );
+    let column_two   = format!( "{0:04b}", hour_now[1..2].parse::<i8>().unwrap() );
+    let column_three = format!( "{0:04b}", minute_now[0..1].parse::<i8>().unwrap() );
+    let column_four  = format!( "{0:04b}", minute_now[1..2].parse::<i8>().unwrap() );
+    let column_five  = format!( "{0:04b}", second_now[0..1].parse::<i8>().unwrap() );
+    let column_six   = format!( "{0:04b}", second_now[1..2].parse::<i8>().unwrap() );
 
-    let b1_1 = stroke_color( c1[0..1].to_string() );
-    let b1_2 = stroke_color( c1[1..2].to_string() );
-    let b1_3 = stroke_color( c1[2..3].to_string() );
-    let b1_4 = stroke_color( c1[3..4].to_string() );
+    let b1_1 = stroke_color( column_one[0..1].to_string() );
+    let b1_2 = stroke_color( column_one[1..2].to_string() );
+    let b1_3 = stroke_color( column_one[2..3].to_string() );
+    let b1_4 = stroke_color( column_one[3..4].to_string() );
 
-    let b2_1 = stroke_color( c2[0..1].to_string() );
-    let b2_2 = stroke_color( c2[1..2].to_string() );
-    let b2_3 = stroke_color( c2[2..3].to_string() );
-    let b2_4 = stroke_color( c2[3..4].to_string() );
+    let b2_1 = stroke_color( column_two[0..1].to_string() );
+    let b2_2 = stroke_color( column_two[1..2].to_string() );
+    let b2_3 = stroke_color( column_two[2..3].to_string() );
+    let b2_4 = stroke_color( column_two[3..4].to_string() );
 
-    let b3_1 = stroke_color( c3[0..1].to_string() );
-    let b3_2 = stroke_color( c3[1..2].to_string() );
-    let b3_3 = stroke_color( c3[2..3].to_string() );
-    let b3_4 = stroke_color( c3[3..4].to_string() );
+    let b3_1 = stroke_color( column_three[0..1].to_string() );
+    let b3_2 = stroke_color( column_three[1..2].to_string() );
+    let b3_3 = stroke_color( column_three[2..3].to_string() );
+    let b3_4 = stroke_color( column_three[3..4].to_string() );
 
-    let b4_1 = stroke_color( c4[0..1].to_string() );
-    let b4_2 = stroke_color( c4[1..2].to_string() );
-    let b4_3 = stroke_color( c4[2..3].to_string() );
-    let b4_4 = stroke_color( c4[3..4].to_string() );
+    let b4_1 = stroke_color( column_four[0..1].to_string() );
+    let b4_2 = stroke_color( column_four[1..2].to_string() );
+    let b4_3 = stroke_color( column_four[2..3].to_string() );
+    let b4_4 = stroke_color( column_four[3..4].to_string() );
 
-    let b5_1 = stroke_color( c5[0..1].to_string() );
-    let b5_2 = stroke_color( c5[1..2].to_string() );
-    let b5_3 = stroke_color( c5[2..3].to_string() );
-    let b5_4 = stroke_color( c5[3..4].to_string() );
+    let b5_1 = stroke_color( column_five[0..1].to_string() );
+    let b5_2 = stroke_color( column_five[1..2].to_string() );
+    let b5_3 = stroke_color( column_five[2..3].to_string() );
+    let b5_4 = stroke_color( column_five[3..4].to_string() );
 
-    let b6_1 = stroke_color( c6[0..1].to_string() );
-    let b6_2 = stroke_color( c6[1..2].to_string() );
-    let b6_3 = stroke_color( c6[2..3].to_string() );
-    let b6_4 = stroke_color( c6[3..4].to_string() );
+    let b6_1 = stroke_color( column_six[0..1].to_string() );
+    let b6_2 = stroke_color( column_six[1..2].to_string() );
+    let b6_3 = stroke_color( column_six[2..3].to_string() );
+    let b6_4 = stroke_color( column_six[3..4].to_string() );
 
     let point = Point::new(3.0, 3.0);
     let point1_2 = Point::new(3.0, 9.0);
@@ -226,16 +226,16 @@ fn main() {
         .with_stroke_width(1.3)
         .with_fill_opacity(0.);
 
-    let datasvg = &svg.to_string();
-    let base64 = encode(datasvg);
+    let data_svg = &svg.to_string();
+    let base_64 = encode(data_svg);
 
-    println!("| image={}", base64);
+    println!("| image={}", base_64);
 }
 
 fn stroke_color(num: String) -> &'static str {
-    if num == "0" {
-        return "gray";
+    return if num == "0" {
+        "gray"
     } else {
-        return "white";
+        "white"
     }
 }
